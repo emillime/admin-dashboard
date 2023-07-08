89ebbb54705f96b3fcd0a6395e6f466588f7bf87
Emil <emil.raksanyi@gmail.com>
2023-07-08 14:38:02 +0200

    Populate db with populate event

--- src/lib/localDb.ts
+++ src/lib/localDb.ts
@@ -1,14 +1,40 @@
-import Dexie, { type Table } from 'dexie';
+import Dexie, { type Table } from "dexie";
+import { getTokenFromCookie } from "../utils/jwtUtils";
+import { getAllBookings } from "$lib/api";
+import { DateTime } from "luxon";
 
 export class MySubClassedDexie extends Dexie {
-	bookings!: Table<Booking>;
+  bookings!: Table<Booking>;
 
-	constructor() {
-		super('operatorDatabase');
-		this.version(1).stores({
-			bookings: '_id, bookingId.orderNumber, startTime, endTime, productSlotId.slot' // Primary key and indexed props
-		});
-	}
+  constructor() {
+    super("operatorDatabase");
+    this.version(1).stores({
+      bookings:
+        "_id, bookingId.orderNumber, startTime, endTime, productSlotId.slot", // Primary key and indexed props
+    });
+  }
 }
 
 export const localDb = new MySubClassedDexie();
+
+localDb.on("populate", async () => {
+  let token = getTokenFromCookie();
+
+  if (token == null || token.length === 0) {
+    console.error("No token found");
+    return;
+  }
+  if (token && token.length > 0) {
+    console.log("Fetching bookings");
+    const bookings: Booking[] = await getAllBookings(
+      token,
+      DateTime.now().startOf("year").toJSDate().toISOString(),
+    );
+    try {
+      const id = await localDb.bookings.bulkPut(bookings);
+      console.log(`Added bookings`);
+    } catch (error) {
+      console.error(`Failed to add bookings: ${error}`);
+    }
+  }
+});
